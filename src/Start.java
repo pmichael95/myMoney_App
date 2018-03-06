6f7a272633d7927e51ecab3702fdc97d4377d3ec
Philip Michael <philmichaelmusic@gmail.com>
2018-03-05 22:41:04 -0500

    Added GUI and launch support for it. Added a WIP GuiView.java class. Slight edits to other files for legibility.

--- src/Start.java
+++ src/Start.java
@@ -1,6 +1,11 @@
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
-
+import javafx.application.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import controllers.DisplayBalanceController;
 import controllers.DepositMoneyController;
 import controllers.WithdrawMoneyController;
@@ -12,23 +17,26 @@ import models.DisplayBalanceModel;
 import models.WithdrawMoneyModel;
 import models.DepositMoneyModel;
 
+import views.GUIView;
+
 // import models.ExampleModel;
-
-
 
 /**
  * Start is the main entry point to the software
  * 
  * @author Steven Tucci
- * @modifiedBy Johnny Mak, Sabrina D'Mello, Shunyu Wang
+ * @modifiedBy Johnny Mak, Sabrina D'Mello, Shunyu Wang, Philip Michael
  * @created 1/29/2018
- * @lastUpdated 2/11/2018 
+ * @lastUpdated 3/5/2018 
  */
-public class Start {
+public class Start extends Application {
 	
 	public static void main(String[] args) {
 		// Get a connection to our database before we start any ui
 		JdbcConnectionSource source = DatabaseConnectionSource.getConnection();
+		
+		// Launches the GUI
+		launch(args);
 		
 		if (source != null) {
 			//ExampleController test = new ExampleController();
@@ -39,11 +47,22 @@ public class Start {
 			
 			DisplayBalanceController dispaly_balance = new DisplayBalanceController();
 			
+			
 			// Once we are done, close the connection to the database 
 			DatabaseConnectionSource.closeConncetion();
 		} else {
 			System.out.println("Could not make a connection to the database");
 		}
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		System.out.println("start called");
+		Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+		primaryStage.setTitle("MyMoney Application");
+		primaryStage.setScene(new Scene(root, 650, 400));
+		primaryStage.setResizable(false); // Prevent fullscreen and drag to resize
+		primaryStage.show();
 	}
 
 }
